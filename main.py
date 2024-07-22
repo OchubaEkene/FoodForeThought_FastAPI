8bd3f7c6b28d19a178896f5e0e4c8228e1e47a1c
Ochuba Ekene Alexander <147551927+OchubaEkene@users.noreply.github.com>
2024-07-22 21:56:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+import generate_mealplan as mp
+import json
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
+@app.get("/get_mealplan/")
+def getMealPlan(tribe: str, state: str, age: int, gender:str):
+    try:
+        results = mp.createMealPlan(tribe, state, age, gender)
+        return json.dumps(results,default=str)
+    except Exception as e:
+        raise e
     return "Hello, World"
 
 if __name__=='__main__':
