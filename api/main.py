5c3d4d9d45e9fd5ac6628a64e895dd8c00d3a952
Ochuba Ekene Alexander <147551927+OchubaEkene@users.noreply.github.com>
2024-08-02 12:36:27 +0100

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import generate_mealplan as mp
+import json
+
+app = FastAPI()
+
+@app.get("/get_mealplan/")
+def getMealPlan(tribe: str, state: str, age: int, gender:str):
+    try:
+        results = mp.createMealPlan(tribe, state, age, gender)
+        return json.dumps(results,default=str)
+    except Exception as e:
+        raise e
+    return "Hello, World"
+
+if __name__=='__main__':
+    import uvicorn
+    uvicorn.run(app)
